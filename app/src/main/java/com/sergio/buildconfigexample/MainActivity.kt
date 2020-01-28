77d1c466cd2b5286b0980940b5d237766cf5dc61
Sergio <lokomouch@gmail.com>
2020-01-28 09:20:47 -0500

    first commit

---
+++ app/src/main/java/com/sergio/buildconfigexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sergio.buildconfigexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("VARIANTES",BuildConfig.BUILD_VARIAN)
+        Log.d("VARIANTES",BuildConfig.BASE_URL)
+
+
+    }
+}
